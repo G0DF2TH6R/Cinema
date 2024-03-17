f366c4fcd9bf40cf75b750d04b03a35f7aedbb36
Chris Matsiselts <85128734+G0DF2TH6R@users.noreply.github.com>
2024-03-17 20:21:46 +0200

    Adding initial models

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -1,13 +1,24 @@
 package org.example.backend;
 
+
+import org.example.backend.Movie.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(MovieRepository movieRepository) {
+        return args -> {
+        };
     }
 
 }
---
+++ backend/src/main/java/org/example/backend/Movie/MovieRepository.java
@@ -0,0 +1,7 @@
+package org.example.backend.Movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
